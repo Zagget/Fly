647d3186453e0f4d49f4d9e70bd6c28bbc9304dd
Lollebollen <joel.reine.larsson@gmail.com>
2025-09-08 15:51:48 +0200

    Destructible walls

--- Assets/Scripts/environment/Damageable.cs
+++ Assets/Scripts/environment/Damageable.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 public abstract class Damageable : MonoBehaviour
 {
     [SerializeField] protected float health;
-    public abstract void Damage(float damage);
+    public abstract void Damage(float damage, Vector3 point, Vector3 force);
 }
