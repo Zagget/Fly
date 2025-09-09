3cb37c981ad433b33577eabb24dc419e4a25a5a9
AxelRK32 <114733972+AxelRK32@users.noreply.github.com>
2025-09-09 12:04:54 +0200

    Added eraser prefab + gizmos

--- Assets/Scripts/Interactables/PenTip.cs
+++ Assets/Scripts/Interactables/PenTip.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class PenTip : MonoBehaviour
 {
     [SerializeField] Transform drawPoint;
-    [SerializeField] float rayLength = 0.5f;
+    [SerializeField] float rayLength = 0.1f;
     [SerializeField] Color drawColor;
-    [SerializeField] int brushSize;
+    [Range(0, 20)][SerializeField] int brushSize;
     void OnCollisionStay(Collision other)
     {
         if (other.gameObject.TryGetComponent<DrawingBoard>(out DrawingBoard drawBoard))
@@ -20,6 +20,11 @@ public class PenTip : MonoBehaviour
 
     void OnDrawGizmos()
     {
-        Gizmos.DrawRay(drawPoint.position, transform.up * rayLength);
+        if (drawPoint)
+        {
+            Gizmos.DrawRay(drawPoint.position, transform.up * rayLength);
+            Gizmos.color = drawColor;
+            Gizmos.DrawSphere(drawPoint.position + transform.up * rayLength, brushSize * 0.03f);
+        }
     }
 }
