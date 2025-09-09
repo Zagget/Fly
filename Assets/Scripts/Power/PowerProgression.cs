5ec6bbf1b4eb1f3509d7a8d21eec196ad47322ec
Lollebollen <joel.reine.larsson@gmail.com>
2025-09-09 11:50:09 +0200

    power progression integration

--- Assets/Scripts/Power/PowerProgression.cs
+++ Assets/Scripts/Power/PowerProgression.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System;
+using UnityEngine.InputSystem;
+
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -9,12 +11,17 @@ public class PowerProgressionEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        PowerProgression powerProgression = (PowerProgression)target;
         base.OnInspectorGUI();
         if (GUILayout.Button("Set debug as real power"))
         {
-            PowerProgression powerProgression = (PowerProgression)target;
             powerProgression.ChangePower(powerProgression.debugPower);
             powerProgression.SetDebugPower();
+        }
+
+        if (GUILayout.Button("Next power"))
+        {
+            powerProgression.NextPower(new());
         }
     }
 }
@@ -46,6 +53,17 @@ public class PowerProgression : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnEnable()
+    {
+        InputManager.Instance.r_ButtonAAction.started += NextPower;
+    }
+
+    private void OnDisable()
+    {
+        InputManager.Instance.r_ButtonAAction.started -= NextPower;
+    }
+
     /// <summary>
     /// Add energy so the player can level up and unlock new powers, each int is a new power.
     /// </summary>
@@ -55,6 +73,7 @@ public class PowerProgression : MonoBehaviour
         energyLevel += amount;
         CheckUnlockReqs();
     }
+
     /// <summary>
     /// Checks if the player can unlock a new power.
     /// </summary>
@@ -63,8 +82,21 @@ public class PowerProgression : MonoBehaviour
         if (Mathf.FloorToInt(energyLevel) > powerLevel)
         {
             powerLevel++;
+            powerLevel = Mathf.Clamp(powerLevel, 0, sortedPowers.Count - 1);
         }
     }
+
+    /// <summary>
+    /// Checks for unlocks after setting new energy level
+    /// </summary>
+    /// <param name="energy"></param>
+    public void SetEnergyLevel(float energy)
+    {
+        energyLevel = energy;
+        CheckUnlockReqs();
+
+    }
+
     /// <summary>
     /// Returns true if the power was successfully changed, false otherwise.
     /// </summary>
@@ -79,6 +111,13 @@ public class PowerProgression : MonoBehaviour
         }
 
         return false;
+    }
+
+    public void NextPower(InputAction.CallbackContext context)
+    {
+        int indexOfPower = sortedPowers.IndexOf(currentPower);
+        currentPower = sortedPowers[(indexOfPower + 1) % (powerLevel + 1)];
+        Debug.Log(currentPower);
     }
 
     /// <summary>
