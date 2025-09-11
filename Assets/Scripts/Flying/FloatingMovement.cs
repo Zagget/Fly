27fe389e314472406f49271ee92a64887065b8cb
sim7234 <simon.olander.tenwolde@hotmail.se>
2025-09-11 14:31:41 +0200

    Updated PlayerController prefab with Floating movement settings, and refactored floating movement a bit.

--- Assets/Scripts/Flying/FloatingMovement.cs
+++ Assets/Scripts/Flying/FloatingMovement.cs
@@ -5,32 +5,40 @@ public class FloatingMovement : MonoBehaviour
 {
     private Rigidbody rb;
 
-    [SerializeField] private float maxVerticalVel;
-    [SerializeField] private float verticalDecceleration;
-    [SerializeField] private float verticalAcceleration;
-
-    [SerializeField] private float acceleration;
     [SerializeField] private float fixedSpeed;
-    [SerializeField] private float decceleration;
+    [SerializeField] private float highSpeedMode;
 
-    [SerializeField] private float tempYOffset;
-    [SerializeField] private float tempZOffset;
+    [SerializeField] private float controllerYOffset = 100; //Offset is aproxematly from ground, where ~0 is the floor.
+    [SerializeField] private float controllerZOffset; //offset is how close controllers have to be to the player to count as 0 or negative.
 
     [SerializeField] private float deadZone;
 
-    [SerializeField] Camera vrCam;
+    private Camera vrCam;
 
     private Vector3 leftController;
     private Vector3 rightController;
 
-    Vector3 linVel; //rb.linearVelocity
+    private Vector3 linVel; //rb.linearVelocity
 
-    Camera activeCamera = null;
+    private Camera activeCamera = null;
 
-    Vector2 horizontalVelocity;
     [SerializeField] private float maxSpeed = 50;
 
-    bool usingVr = false;
+    private float SMLeftMinimum = 0.1f;
+    private float SMRightMinimum = 0.3f;
+
+    Vector3 controllerPositionInput;
+
+    private float turnStrength;
+    private Vector2 rightAxisInput;
+
+    private FlightMode currentMode;
+    private enum FlightMode
+    {
+        Standard = 0,
+        Fast = 1,
+    }
+
 
     private void Start()
     {
@@ -39,7 +47,6 @@ public class FloatingMovement : MonoBehaviour
 
         if (RigManager.instance.usingVr == true)
         {
-            usingVr = true;
             activeCamera = vrCam;
         }
         else
@@ -53,69 +60,92 @@ public class FloatingMovement : MonoBehaviour
     {
         linVel = rb.linearVelocity;
 
-        //VerticalFlight();
-        ForwardFlight();
+        controllerPositionInput = GetControllerPositions();
+        currentMode = GetFlightMode();
+        switch (currentMode)
+        {
+            case FlightMode.Standard:
+                StandardControls();
+                break;
+
+            case FlightMode.Fast:
+                FastControls();
+                break;
+        }
+
         rb.linearVelocity = linVel;
     }
 
-    private void ForwardFlight()
+    private FlightMode GetFlightMode()
     {
-
-        float xValue = 0;
-        float yValue = 0;
-        float zValue = 0;
-        if (usingVr)
+        if (rightAxisInput != Vector2.zero) //Super man pose.
         {
-            leftController = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LHand);
-            rightController = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RHand);
-
-            rightController.y -= 1; // this makes the controller be tracked more accurately as 0 was floor before.
-            leftController.y -= 1;
-
-            xValue += leftController.x * 100;
-            xValue += rightController.x * 100;
-            xValue = xValue / 2;
-
-            yValue = ((leftController.y + rightController.y) / 2) * 100;
-            //yValue -= tempYOffset;
-
-            zValue += leftController.z * 100;
-            zValue += rightController.z * 100;
-            zValue = (zValue / 2) - tempZOffset;
-
-           // if (zValue < 0) zValue = 0;
-
-            if (Mathf.Abs(xValue) < deadZone) xValue = 0;
-
-            if (Mathf.Abs(yValue) < deadZone) yValue = 0;
-
-            if (Mathf.Abs(zValue) < deadZone) zValue = 0;
+            return FlightMode.Fast;
         }
+        else
+        {
+            return FlightMode.Standard;
+        }
+    }
 
-        Vector3 inputDirection = new Vector3(xValue, yValue, zValue).normalized;
+    private void StandardControls()
+    {
+        Vector3 inputDirection = controllerPositionInput.normalized;
 
         linVel = Vector3.zero;
 
-        linVel += RigManager.instance.currentRb.transform.forward * Time.fixedDeltaTime * fixedSpeed * zValue;
-        linVel += RigManager.instance.currentRb.transform.up * Time.fixedDeltaTime * fixedSpeed * yValue;
-        linVel += RigManager.instance.currentRb.transform.right * Time.fixedDeltaTime * fixedSpeed * xValue;
+        linVel += RigManager.instance.currentRb.transform.forward * Time.fixedDeltaTime * fixedSpeed * controllerPositionInput.z;
+        linVel += RigManager.instance.currentRb.transform.up * Time.fixedDeltaTime * fixedSpeed * controllerPositionInput.y;
+        linVel += RigManager.instance.currentRb.transform.right * Time.fixedDeltaTime * fixedSpeed * controllerPositionInput.x;
+    }
 
-        if (leftController.z <= 0.1f && rightController.z > 0.3f)
-        {
-            linVel += RigManager.instance.currentRb.transform.forward * Time.fixedDeltaTime * fixedSpeed * zValue * 10;
-        }
+    private Vector3 GetControllerPositions()
+    {
+        float xValue = 0;
+        float yValue = 0;
+        float zValue = 0;
+
+        leftController = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LHand);
+        rightController = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RHand);
+
+
+        xValue = ((leftController.x + rightController.x) / 2) * 100;
+
+        yValue = ((leftController.y + rightController.y) / 2) * 100;
+        yValue -= controllerYOffset;
+
+        zValue = ((leftController.z + rightController.z) / 2) * 100;
+        zValue -= controllerZOffset;
+
+        // if (zValue < 0) zValue = 0;
+
+        if (Mathf.Abs(xValue) < deadZone) xValue = 0;
+
+        if (Mathf.Abs(yValue) < deadZone) yValue = 0;
+
+        if (Mathf.Abs(zValue) < deadZone) zValue = 0;
+
 
         Debug.Log(" X: " + xValue + " " + " Y: " + yValue + " " + " Z: " + zValue);
 
-        //if (Vector3.Dot(linVel, activeCamera.transform.forward) < 0)
-        //{
-        //    linVel += activeCamera.transform.forward * Time.fixedDeltaTime * decceleration;
+        return new Vector3(xValue, yValue, zValue);
+    }
 
-        //}
+    private void FastControls()
+    {
+        Vector3 forward = RigManager.instance.currentRb.transform.forward;
+        Vector3 right = RigManager.instance.currentRb.transform.right;
+
+        
+
+        Vector3 moveDirection = forward;
+        moveDirection = Vector3.Lerp(moveDirection, rightAxisInput.normalized * forward, Time.fixedDeltaTime * turnStrength);
+        linVel = moveDirection * highSpeedMode;
     }
 
     public void FlyingInput(InputAction.CallbackContext context)
     {
         Vector2 input = context.ReadValue<Vector2>();
+        rightAxisInput = input;
     }
 }
