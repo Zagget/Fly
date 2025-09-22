833045bc32438a6c924e1fe5a6ce67a30ae5c134
Jonathan Romskaug <jonathan.romskaug@gmail.com>
2025-09-22 13:54:52 +0200

    +Canvas dosent follow player when in menu.

--- Assets/Scripts/UI/FollowPlayersVision.cs
+++ Assets/Scripts/UI/FollowPlayersVision.cs
@@ -4,18 +4,42 @@ public class FollowPlayersVision : MonoBehaviour
 {
     [SerializeField] float distance;
     [SerializeField] float slerpAmount;
-    
+
     Transform cameraTransform;
     Vector3 currentDirection = Vector3.up;
+
+    private BasePlayerState menu;
+    private bool followPlayer = true;
 
     private void Start()
     {
         cameraTransform = RigManager.instance.eyeAnchor;
         currentDirection = cameraTransform.forward;
+
+        menu = StateManager.Instance.menuState;
+
+        StateManager.Instance.OnStateChanged += CheckForMenuState;
+    }
+
+    void OnDisable()
+    {
+        StateManager.Instance.OnStateChanged -= CheckForMenuState;
+    }
+
+    private void CheckForMenuState(BasePlayerState newState, BasePlayerState oldState)
+    {
+        if (newState == menu)
+        {
+            followPlayer = false;
+            return;
+        }
+        followPlayer = true;
     }
 
     private void Update()
     {
+        if (!followPlayer) return;
+
         currentDirection = Vector3.Slerp(currentDirection, cameraTransform.forward, slerpAmount * Time.deltaTime);
         transform.rotation = cameraTransform.rotation;
         transform.position = cameraTransform.position + currentDirection * distance;
