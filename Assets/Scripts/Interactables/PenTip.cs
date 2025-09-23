ece46f4835609e6e4f60b86ba4e886119d841926
AxelRK32 <114733972+AxelRK32@users.noreply.github.com>
2025-09-23 10:40:28 +0200

    Changed pen to use trigger instead of collision

--- Assets/Scripts/Interactables/PenTip.cs
+++ Assets/Scripts/Interactables/PenTip.cs
@@ -9,13 +9,13 @@ public class PenTip : MonoBehaviour
     private float drawCooldown = 0.05f;
     private float lastDrawTime = 0f;
 
-    void OnCollisionStay(Collision other)
+    void OnTriggerStay(Collider other)
     {
         if (Time.time - lastDrawTime < drawCooldown) return;
 
         if (other.gameObject.TryGetComponent<DrawingBoard>(out DrawingBoard drawBoard))
         {
-            Ray ray = new Ray(drawPoint.position, -other.contacts[0].normal);
+            Ray ray = new Ray(drawPoint.position, drawPoint.up);
             if (Physics.Raycast(ray, out RaycastHit hit, rayLength))
             {
                 drawBoard.DrawAtPosition(hit.textureCoord, drawColor, brushSize);
@@ -23,12 +23,26 @@ public class PenTip : MonoBehaviour
             }
         }
     }
+    // void OnCollisionStay(Collision other)
+    // {
+    //     if (Time.time - lastDrawTime < drawCooldown) return;
+
+    //     if (other.gameObject.TryGetComponent<DrawingBoard>(out DrawingBoard drawBoard))
+    //     {
+    //         Ray ray = new Ray(drawPoint.position, -other.contacts[0].normal);
+    //         if (Physics.Raycast(ray, out RaycastHit hit, rayLength))
+    //         {
+    //             drawBoard.DrawAtPosition(hit.textureCoord, drawColor, brushSize);
+    //             lastDrawTime = Time.time;
+    //         }
+    //     }
+    // }
 
     void OnDrawGizmosSelected()
     {
         if (drawPoint)
         {
-            Gizmos.DrawRay(drawPoint.position, transform.up * rayLength);
+            Gizmos.DrawRay(drawPoint.position, drawPoint.up * rayLength);
             Gizmos.color = drawColor;
             Gizmos.DrawSphere(drawPoint.position + transform.up * rayLength, brushSize * 0.03f);
         }
