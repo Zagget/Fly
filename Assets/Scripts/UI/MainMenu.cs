0ec8eb1cd0b74fb67e050e0c4060bab6b54b4c64
sim7234 <simon.olander.tenwolde@hotmail.se>
2025-10-08 12:43:05 +0200

    Main menu WIP

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,59 @@
+using UnityEditor;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject[] UIToDisableWithCredits;
+    [SerializeField] private GameObject UIToEnableWithCredits;
+    [SerializeField] private MenuUI menuUI;
+
+    private PlayerController playerController;
+
+    private void Start()
+    {
+        playerController = PlayerController.Instance;
+       // playerController.SetState(StateManager.Instance.mainMenuState);
+        StateManager.Instance.OnStateChanged += OnStateChanged;
+    }
+
+    private void OnStateChanged(BasePlayerState newState, BasePlayerState lastState)
+    {
+        if (newState == StateManager.Instance.mainMenuState)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+
+    public void PressPlay()
+    {
+        playerController.SetState(StateManager.Instance.flyingState);
+        StateManager.Instance.OnStateChanged -= OnStateChanged;
+        gameObject.SetActive(false);
+    }
+
+    public void PressSettings()
+    {
+        gameObject.SetActive(false);
+        menuUI.EnterMenu();
+    }
+
+    public void PressCredits()
+    {
+        foreach (GameObject go in UIToDisableWithCredits)
+        {
+            go.SetActive(!go.activeSelf);
+        }
+
+        UIToEnableWithCredits.SetActive(!UIToEnableWithCredits.activeSelf);
+    }
+
+    public void PressQuit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+}
